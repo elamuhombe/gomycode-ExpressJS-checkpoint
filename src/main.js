8feef38cbe8c1474aceb79c5ab46131e964951dd
Elaine Muhombe <emuhombe@gmail.com>
2024-03-09 14:09:11 +0300

    chore: modify the directory for static files

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const workingHoursMiddleware = (req, res, next) => {
     const hour = now.getHours();
 
     // Check if it's a weekday (Monday to Friday) and between 9am and 5pm
-    if (day >= 1 && day <= 5 && hour >= 6 && hour < 23) {
+    if (day >= 1 && day <= 6 && hour >= 6 && hour < 23) {
         next(); // Proceed to the next middleware/route handler
     } else {
         res.status(403).send('Sorry, the app is only accessible during working hours (9am to 5pm) from Monday to Friday.');
@@ -25,9 +25,15 @@ app.use(workingHoursMiddleware);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
 
+
 // Define the directory for static files
-const publicDirectoryPath = path.join(__dirname,  'public');
+const publicDirectoryPath = path.join(__dirname, '..', 'public'); // Move up one directory to access public directory
+const srcDirectoryPath = path.join(__dirname);
+
+// Configure Express to serve static files from the specified directories
 app.use(express.static(publicDirectoryPath));
+app.use(express.static(srcDirectoryPath));
+
 
 // Define a route handler for the root URL
 app.get('/', (req, res) => {
